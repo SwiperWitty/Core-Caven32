e6224ae6ddb4d9bbefec544a3c79552f9fbcad82
SwiperWitty <swiper_witty@qq.com>
2024-02-06 18:36:12 +0800

    open power

--- DIY-Demo/OPEN_Nc_Power/User/Main.c
+++ DIY-Demo/OPEN_Nc_Power/User/Main.c
@@ -15,16 +15,41 @@
 #define STEE_APP  0x03 // 舵机
 #define GAMES_APP 0x04 // Games
 
+#define YG_DEFAULT	50
+#define YG_DIF_MIN	10
+#define YG_DIF_MAX	45
+
 int temp = 0;
+
+int yg_lock = 0; 
+
+u16 ADC_array[10];
+
+int Val_YG_x,Val_YG_y;
+
+float Val_array[10];
+
+float Val_temp_f;
+     
+float Val_OUT_array[10];
+float Val_ELE_array[10];
+float Val_TEM_array[10];
+char Val_OUT_num = 0;
+char Val_ELE_num = 0;
+char Val_TEM_num = 0;
+
+int show_max_x = 20;
+int show_max_y = 10;
 char array_buff[300];
 Caven_App_Type Open_power = {
     .app_ID = 0x01,
     .p_Data = NULL,
     .string = NULL,
 };
-
+Caven_Control_Type YG_Control;
 void Main_Init(void);
-void LCD_Show_string(char *string,char width);
+void LCD_Show_string(char *string,char cursor,char width,char length);
+void ADC_Data_Handle (void * data);
 int Home_app (Caven_App_Type * message);
 /*
     Home_app
@@ -38,9 +63,58 @@ int main(void)
     Main_Init();
     
     Open_power.string = array_buff;
-    
+    Open_power.p_Data = &YG_Control;
     while (1) 
     {
+        int temp = 0,num = 0;
+        char array_buff[300];
+        Val_array[temp++]  = (0x0fff - ADC_array[num++]) / 40;       // 0
+        Val_array[temp++] = ADC_array[num++] / 40;
+
+        //
+        Val_YG_x = Val_array[0];
+        Val_YG_y = Val_array[1];
+        temp = Val_YG_x - YG_DEFAULT;
+        if ((temp > YG_DIF_MIN) && (yg_lock > 0))		// 左
+        {
+            YG_Control.Control_x = (-1);
+            yg_lock = 0;
+        }
+        else if ((-temp > YG_DIF_MIN) && (yg_lock > 0))	// 右
+        {
+            YG_Control.Control_x = (1);
+            yg_lock = 0;
+        }
+        else if(abs(temp) < YG_DIF_MIN)					// 空闲　
+        {
+            yg_lock++;
+            yg_lock = MIN(yg_lock,100);
+        }
+        temp = Val_YG_y - YG_DEFAULT;
+        if ((temp > YG_DIF_MIN) && (yg_lock > 0))		// 上
+        {
+            YG_Control.Control_y = (-1);
+            yg_lock = 0;
+        }
+        else if ((-temp > YG_DIF_MIN) && (yg_lock > 0))	// 下
+        {
+            YG_Control.Control_y = (1);
+            yg_lock = 0;
+        }
+        else if(abs(temp) < YG_DIF_MIN)					// 空闲　
+        {
+            yg_lock++;
+            yg_lock = MIN(yg_lock,100);
+        }
+        
+        // 
+        if (YG_KEY_STATE() == 0)
+        {
+            YG_Control.Control_botton = 1;
+            do{
+                
+            }while(YG_KEY_STATE() == 0);
+        }
         switch (Open_power.app_ID) 
         {
             case HOME_APP:
@@ -58,8 +132,8 @@ int main(void)
             default:
                 break;
         }
-        if (Open_power.string != NULL) {
-            LCD_Show_string(Open_power.string,20);
+        if (Open_power.string != NULL && Open_power.str_switch == 1) {
+            LCD_Show_string(Open_power.string,Open_power.cursor,show_max_x,show_max_y);
         }
 
         if (retval) {
@@ -79,6 +153,9 @@ void Main_Init(void)
     Mode_Init.TIME_Init_State = Mode_Init.TIME(ENABLE);
     Mode_Init.UART_Init_State = Mode_Init.UART(DEBUG_OUT, 115200, ENABLE);
     Mode_Init.LCD_Init_State = Mode_Init.LCD(ENABLE);
+    
+    Mode_Init.User_ADC(ENABLE);
+	Mode_Use.USER_ADC.Receive_Bind_pFun(ADC_Data_Handle);
     //    Mode_Init.Steering_Engine(ENABLE);
 
     reverse |= Power_app_init(ENABLE);
@@ -94,14 +171,58 @@ void Main_Init(void)
 #endif
 }
 
+
+
 int Home_app (Caven_App_Type * message)
 {
     int retval = 0;
-    int aaa;
+    int run_num = 0;
+    static int first = 1;
+    Caven_Control_Type control;
+
     if (message != NULL)
     {
-        memcpy(aaa,message.p_Data,sizeof(aaa));
+        memcpy(&control,message->p_Data,sizeof(control));
+        memset(message->string,0,50);
+        if(first)
+        {
+            message->str_switch = 1;
+            message->cursor = 0;
+            message->layer = first;
+            first = 0;
+        }
+        if(control.Control_y > 0)
+        {
+            message->cursor ++;
+        }
+        else if(control.Control_y < 0)
+        {
+            message->cursor --;
+        }
+        control.Control_y = 0;
+        message->cursor = MAX(message->cursor,2);       // 第一个可选是2
+        if(message->cursor > 5)
+        {
+            message->cursor = show_max_y -1;            // 最后一个可选是5，跳转到break 
+        }
         
+        sprintf(message->string,"Caven Open power");            // 0
+        run_num += sizeof("Caven Open power");
+        if(message->layer == 1)
+        {
+            sprintf(message->string + run_num," ");             // 1
+            run_num += sizeof(" ");
+            sprintf(message->string + run_num,"DC-DC Power");   // 2
+            run_num += sizeof("DC-DC Power");
+            sprintf(message->string + run_num,"Steer Control"); // 3
+            run_num += sizeof("Steer Control");
+            sprintf(message->string + run_num,"Games");         // 4
+            run_num += sizeof("Games");
+            sprintf(message->string + run_num,"About");         // 5
+            run_num += sizeof("About");
+            sprintf(message->string + run_num,"\nbreak[user/home]");    // end(6)
+
+        }
     }
     
     return retval;
@@ -109,13 +230,15 @@ int Home_app (Caven_App_Type * message)
 /*
 string_a = " ",end is \0,so strlen(string_a) = 1.
 */
-void LCD_Show_string(char *string,char width)
+void LCD_Show_string(char *string,char cursor,char width,char length)
 {
     char temp_str[50];
+    char *p_str;
     int run_num = 0;
     int get_num;
     int show_point_x,show_point_y = 0;
-    if (string == NULL)
+    int color;
+    if (string == NULL && width > 0 && length > 0)
     {
         return;
     }
@@ -134,13 +257,45 @@ void LCD_Show_string(char *string,char width)
         {
             show_point_x = (width - get_num) / 2;
         }
+        p_str = temp_str;
+        if(temp_str[0] == '\n')
+        {
+            show_point_x = 0;
+            show_point_y = length - 1;
+            p_str = &temp_str[1];
+        }
+        if(show_point_y == 0)
+        {
+            color = LCD_BLUE;
+        }
+        else if(show_point_y == cursor)
+        {
+            color = LCD_MAGENTA;
+        }
+        else
+        {
+            color = LCD_Word_Color;
+        }
+        
         if(get_num > 0)
         {
-            Mode_Use.LCD.Show_String_pFun(show_point_x, show_point_y++, temp_str, LCD_Word_Color, LCD_Back_Color, 24);
+
+            Mode_Use.LCD.Show_String_pFun(show_point_x, show_point_y, p_str, color, LCD_Back_Color, 24);
+            show_point_y++;
         }
-        if (show_point_y > 10)
+        if (show_point_y > length)
         {
             break;
         }
     } while (get_num);
 }
+
+void ADC_Data_Handle (void * data)
+{
+    memcpy(ADC_array,data,sizeof(ADC_array));
+    if(ADC_array[6] > 2000)
+    {
+        DC_5V_OFF();
+    }
+}
+
--- DIY-Demo/OPEN_Nc_Power/User/power_app.c
+++ DIY-Demo/OPEN_Nc_Power/User/power_app.c
@@ -1,46 +1,25 @@
 #include "power_app.h"
 
-#define YG_DEFAULT	50
-#define YG_DIF_MIN	10
-#define YG_DIF_MAX	45
+
 #define Divider_RES 8.5     // ((75 + 10) / 10) adc * Divider_RES = vcc
 #define MULTIP_RATIO 55.5	// INA119A1
 #define Sampling_RES 0.005	// 0.005Ω
 #define Sampling_RES_RATIO 200.0	// (5/1000)(Ω) -> 200(RATIO)
 
 int show_cycle = 0;
-int yg_lock = 0; 
-
-u16 ADC_array[10];
-float Val_OUT_set = 0,Val_OUT_temp = 6.0;
-float Val_ELE_set = 2.4,Val_ELE_temp = 2.4;
-
-int Val_YG_x,Val_YG_y;
 float Val_IN_f = 0;
 float Val_OUT_f = 0;
 float Val_ELE_f = 0;
 float Val_TEM_f = 0;
-float Val_array[10];
+float Val_OUT_set = 0,Val_OUT_temp = 6.0;
+float Val_ELE_set = 2.4,Val_ELE_temp = 2.4;
 
-float Val_temp_f;
-     
-float Val_OUT_array[10];
-float Val_ELE_array[10];
-float Val_TEM_array[10];
-char Val_OUT_num = 0;
-char Val_ELE_num = 0;
-char Val_TEM_num = 0;
-
-void ADC_Data_Handle (void * data);
 int PD_Set_Mode (char grade);
 void SET_Val_Handle (float set_val,float get_val);
 
 int Power_app_init (int Set)
 {
     int retval = 0;
-	
-	Mode_Init.User_ADC(ENABLE);
-	Mode_Use.USER_ADC.Receive_Bind_pFun(ADC_Data_Handle);
 	
 	TIM3_PWM_Start_Init(1000,12,ENABLE);
 	TIM3_PWMx_SetValue(1,1000);              
@@ -138,68 +117,16 @@ void SET_Val_Handle (float set_val,float get_val)
     }
 }
 
-void ADC_Data_Handle (void * data)
-{
-    memcpy(ADC_array,data,sizeof(ADC_array));
-    if(ADC_array[6] > 2000)
-    {
-        DC_5V_OFF();
-    }
-}
+
 
 int Power_app (Caven_App_Type * message)
 {
     int retval = 0;
 	int temp = 0,num = 0;
 	char array_buff[300];
-	Val_array[temp++]  = (0x0fff - ADC_array[num++]) / 40;       // 0
-	Val_array[temp++] = ADC_array[num++] / 40;
-	Val_array[temp++] = Mode_Use.USER_ADC.Conversion_Vol_pFun(ADC_array[num++]) * Divider_RES;
-	Val_array[temp++] = Mode_Use.USER_ADC.Conversion_Vol_pFun(ADC_array[num++]) * Divider_RES;
-	Val_array[temp++] = ADC_array[num++] / 40;					//ntc
-	Val_array[temp++] = Mode_Use.USER_ADC.Conversion_Vol_pFun(ADC_array[num++]) * (Sampling_RES_RATIO / MULTIP_RATIO);
-	Val_array[temp++] = Mode_Use.USER_ADC.Get_MCU_Temperature_pFun();            // 内温 
+
 	//
-	Val_IN_f = Val_array[2];
-	//
-	Val_YG_x = Val_array[0];
-	Val_YG_y = Val_array[1];
-	temp = Val_YG_x - YG_DEFAULT;
-	if ((temp > YG_DIF_MIN) && (yg_lock > 0))		// 左
-	{
-		yg_lock = 0;
-		Val_OUT_temp += 1.0;
-	}
-	else if ((-temp > YG_DIF_MIN) && (yg_lock > 0))	// 右
-	{
-		yg_lock = 0;
-		Val_OUT_temp -= 1.0;
-	}
-	else if(abs(temp) < YG_DIF_MIN)					// 空闲　
-	{
-		yg_lock++;
-		yg_lock = MIN(yg_lock,100);
-	}
-	Val_OUT_temp = MIN(Val_OUT_temp,Val_IN_f);
-	Val_OUT_temp = MAX(Val_OUT_temp,1);
-	// 
-	if (YG_KEY_STATE() == 0)
-	{
-		Val_OUT_set = Val_OUT_temp;
-		do{
-			
-		}while(YG_KEY_STATE() == 0);
-	}
-	// 
-	temp = Data_Median_filtering_Handle (Val_array[3],Val_OUT_array,&Val_OUT_f,&Val_OUT_num,10);
-	if(temp)
-	{
-		SET_Val_Handle (Val_OUT_set,Val_OUT_f);
-	}
-	temp = Data_Median_filtering_Handle (Val_array[5],Val_ELE_array,&Val_ELE_f,&Val_ELE_num,10);
-	temp = Data_Median_filtering_Handle (Val_array[6],Val_TEM_array,&Val_TEM_f,&Val_TEM_num,10);
-	//
-	Vofa_JustFloat_Show_Fun (Val_array);
+//	Vofa_JustFloat_Show_Fun (Val_array);
 	//
 	show_cycle++;
 	if(show_cycle > 100)	// 选中时颜色 LCD_BRRED，默认颜色 LCD_BLUE，LCD_MAGENTA
