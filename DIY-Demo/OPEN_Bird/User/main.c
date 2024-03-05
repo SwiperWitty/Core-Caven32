5aa7057359afea528a2339268e7a3911d9a7e926
SwiperWitty <swiper_witty@qq.com>
2024-03-05 16:07:53 +0800

    new Draw_Circle

--- DIY-Demo/OPEN_Bird/User/main.c
+++ DIY-Demo/OPEN_Bird/User/main.c
@@ -11,8 +11,7 @@ u8 send_array[64];
 int run_num;
 float float_array[10];
 
-void Draw_Circle(U16 x0, U16 y0, char radius, char wide,char percent,U16 color);
-float test;
+void Draw_Circle(U16 x0,U16 y0,char radius,char wide,char percent,U16 color);
 
 int main(void)
 {
@@ -30,7 +29,7 @@ int main(void)
     };
     Vofa_JustFloat_Init_Fun (2,Debug_Out);
     float_array[1] = 3.3;
-
+    char x_bili = 50;
     while(1)
     {
         now_time = Mode_Use.TIME.Get_Watch_pFun();
@@ -38,15 +37,18 @@ int main(void)
         API_Task_Timer (&LED_Task,now_time);        // LED任务
         Mode_Use.LED.SET_pFun(1,LED_Task.Flip_falg);
 
-
         float_array[0] += 1.133;
         if (float_array[0] > 10) {
             float_array[0] = 0;
         }
 //        Mode_Use.LCD.Draw_Circle_pFun(100,100,50,LCD_BLACK);
         Mode_Use.LCD.Draw_Point_pFun (120,120,LCD_BLACK);
-        Draw_Circle(120, 120, 50, 0,70,LCD_BLACK);
-
+        Draw_Circle(120, 120, 80, 16,x_bili,LCD_BLACK);
+        x_bili += 5;
+        if (x_bili > 100) {
+            x_bili = 0;
+            LCD_Fill_Fun (0, 0, LCD_W, LCD_H, LCD_Back_Color);
+        }
 //        Vofa_JustFloat_Show_Fun (float_array);
         Mode_Use.TIME.Delay_Ms(1000);
     }
@@ -54,71 +56,157 @@ int main(void)
     SYS_RESET();
 }
 
-void Draw_Circle(U16 x0, U16 y0, char radius, char wide,char percent,U16 color)
+void Draw_Circle(U16 x0,U16 y0,char radius,char wide,char percent,U16 color)
 {
     int a, b;
-    a = 0;
-    b = radius;
-    int temp_num = radius * radius;
-    int temp_num_b = b * b;
-    int temp_run = 0;
-    char pic_array[400][2];
+    int temp_num;
+    int temp_num_b;
+    int temp_run;
+    u8 pic_array[256][2];
+    u8 show_x,show_y;
 
-    char xiang_x = 0; //(x/25 + 1);
+    char quadrant = 0;          //
+    char subzone;
     float f_temp_percent;
     float f_temp_num;
     float x_point = 0,y_point = 0;
-    temp_run = percent/25 + 1;
-    switch (temp_run) {
+
+    subzone = percent/25 + 1;
+    switch (subzone) {
         case 1:
-            xiang_x = 1;
+            quadrant = 1;
             break;
         case 2:
-            xiang_x = 4;
+            quadrant = 4;
             break;
         case 3:
-            xiang_x = 3;
+            quadrant = 3;
             break;
         case 4:
-            xiang_x = 2;
+            quadrant = 2;
             break;
         default:
+            quadrant = 5;
             break;
     }
     f_temp_num = percent % 25;
     f_temp_percent = (f_temp_num / 25) * 90;
-    f_temp_num = 3.1415926 / 180.0;
-    f_temp_num *= f_temp_percent;
+    f_temp_num = (f_temp_percent / 180) * 3.1415926;
 
-    x_point = sinf(f_temp_num) * radius;
-    y_point = cosf(f_temp_num) * radius;
-    printf("x : %d %%,xiang x bb: %d,x: %5.2f -> sin x = %5.3f,cos y = %5.3f \n",percent,xiang_x,f_temp_percent,x_point,y_point);
+    do{
+        a = 0;
+        b = radius++;
+        temp_run = 0;
+        temp_num = b * b;
+        temp_num_b = b * b;
 
-    temp_run = 0;
-    while (a <= b)
-    {
-        pic_array[temp_run][0] = x0 + b - 120; pic_array[temp_run++][1] = y0 - a - 120;
-        pic_array[temp_run][0] = x0 + a - 120; pic_array[temp_run++][1] = y0 - b - 120;
-//        temp_run++;
-        a++;
-        if ((a * a + temp_num_b) > temp_num) // 判断要画的点是否过远
+        while (a <= b)      // 画圆（弧）
         {
-            b--;
-            temp_num_b = b * b;
+            pic_array[temp_run][0] = b; pic_array[temp_run++][1] = a;
+            pic_array[temp_run][0] = a; pic_array[temp_run++][1] = b;
+            a++;
+            if ((a * a + temp_num_b) > temp_num) // 判断要画的点是否过远
+            {
+                b--;
+                temp_num_b = b * b;
+            }
         }
-    }
-    for (int i = 0; i < temp_run; i++) {
-        Mode_Use.LCD.Draw_Point_pFun (pic_array[i][0]+120,pic_array[i][1]+120,color);
-    }
-    for (int i = 0; i < temp_run; i++) {
-        Mode_Use.LCD.Draw_Point_pFun (pic_array[i][0]+120,120-pic_array[i][1],color);
-    }
-//    for (int i = 0; i < temp_run; i++) {
-//        Mode_Use.LCD.Draw_Point_pFun (120-pic_array[i][0],pic_array[i][1]+120,color);
-//    }
-    for (int i = 0; i < temp_run; i++) {
-        Mode_Use.LCD.Draw_Point_pFun (120-pic_array[i][0],120-pic_array[i][1],color);
-    }
+    //    for (int i = 0; i < temp_run; i++) {            // 1象限
+    //        Mode_Use.LCD.Draw_Point_pFun (x0+pic_array[i][0],y0-pic_array[i][1],color);
+    //    }
+    //    for (int i = 0; i < temp_run; i++) {            // 2
+    //        Mode_Use.LCD.Draw_Point_pFun (x0-pic_array[i][0],y0-pic_array[i][1],color);
+    //    }
+    //    for (int i = 0; i < temp_run; i++) {            // 3
+    //        Mode_Use.LCD.Draw_Point_pFun (x0-pic_array[i][0],y0+pic_array[i][1],color);
+    //    }
+    //    for (int i = 0; i < temp_run; i++) {            // 4
+    //        Mode_Use.LCD.Draw_Point_pFun (x0+pic_array[i][0],y0+pic_array[i][1],color);
+    //    }
+        if(subzone > 1)
+        {
+            for (int i = 0; i < temp_run; i++) {            // 1
+                show_x = x0+pic_array[i][0];
+                show_y = y0-pic_array[i][1];
+                Mode_Use.LCD.Draw_Point_pFun (show_x,show_y,color);
+            }
+        }
+        else if(subzone == 1)
+        {
+            x_point = sinf(f_temp_num) * radius + x0;
+            y_point = cosf(f_temp_num) * radius + y0;
+            for (int i = 0; i < temp_run; i++) {
+                show_x = x0+pic_array[i][0];
+                show_y = y0-pic_array[i][1];
+                if (show_x <= x_point) {
+                    Mode_Use.LCD.Draw_Point_pFun (show_x,show_y,color);
+                }
+            }
+        }
+        if(subzone > 2)
+        {
+            for (int i = 0; i < temp_run; i++) {
+                show_x = x0+pic_array[i][0];
+                show_y = y0+pic_array[i][1];
+                Mode_Use.LCD.Draw_Point_pFun (show_x,show_y,color);
+            }
+        }
+        else if(subzone == 2)
+        {
+            x_point = cosf(f_temp_num) * radius + x0;
+            y_point = sinf(f_temp_num) * radius + y0;
+            for (int i = 0; i < temp_run; i++) {
+                show_x = x0+pic_array[i][0];
+                show_y = y0+pic_array[i][1];
+                if (show_y <= y_point) {
+                    Mode_Use.LCD.Draw_Point_pFun (show_x,show_y,color);
+                }
+            }
+        }
+        if(subzone > 3)
+        {
+            for (int i = 0; i < temp_run; i++) {
+                show_x = x0-pic_array[i][0];
+                show_y = y0+pic_array[i][1];
+                Mode_Use.LCD.Draw_Point_pFun (show_x,show_y,color);
+            }
+        }
+        else if(subzone == 3)
+        {
+            x_point = sinf(f_temp_num) * radius + x0;
+            y_point = cosf(f_temp_num) * radius + y0;
+            for (int i = 0; i < temp_run; i++) {
+                show_x = x0-pic_array[i][0];
+                show_y = y0+pic_array[i][1];
+                if (show_y >= y_point) {
+                    Mode_Use.LCD.Draw_Point_pFun (show_x,show_y,color);
+                }
+            }
+        }
+        if(subzone > 4)
+        {
+            for (int i = 0; i < temp_run; i++) {
+                show_x = x0-pic_array[i][0];
+                show_y = y0-pic_array[i][1];
+                Mode_Use.LCD.Draw_Point_pFun (show_x,show_y,color);
+            }
+        }
+        else if(subzone == 4)
+        {
+            x_point = cosf(f_temp_num) * radius + x0;
+            y_point = y0 - sinf(f_temp_num) * radius;
+            for (int i = 0; i < temp_run; i++) {
+                show_x = x0-pic_array[i][0];
+                show_y = y0-pic_array[i][1];
+                if (show_y >= y_point) {
+                    Mode_Use.LCD.Draw_Point_pFun (show_x,show_y,color);
+                }
+            }
+        }
+    }while(wide--);
+    printf("x : %d %%,quadrant: %d,x: %5.2f -> sin x = %5.3f,cos y = %5.3f \n",percent,quadrant,f_temp_percent,x_point,y_point);
+    sprintf(send_array,"%3d%% ",percent);
+    Mode_Use.LCD.Show_String_pFun (8,0,send_array,LCD_BLACK,LCD_Back_Color,24);
 }
 
 void Main_Init(void)
