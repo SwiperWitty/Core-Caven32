a5e8d1ef558b4486621710363e235770e23f5eca
Swiper_witty <1750273288@qq.com>
2025-11-24 16:14:40 +0800

    demo cc wch

--- Caven-V3.14-RISC-V/CH32V317VC/User/main.c
+++ Caven-V3.14-RISC-V/CH32V317VC/User/main.c
@@ -4,275 +4,13 @@
  * Version            : V1.0.0
  * Date               : 2025/11/4
  * Description        : Main program body.
-*********************************************************************************
-/*
- *@Note
-TCP Server example, demonstrating that TCP Server
-receives data and sends back after connecting to a client.
-For details on the selection of engineering chips,
-please refer to the "CH32V30x Evaluation Board Manual" under the CH32V307EVT\EVT\PUB folder.
- */
-#include "string.h"
-#include "eth_driver.h"
-#include "IQmath_RV32.h"
-#define KEEPALIVE_ENABLE                1               //Enable keep alive function
+*********************************************************************************/
+
 
 #include "Base_ETH.h"
+#include "Base_TCP_Server.h"
+#include "Base_TCP_Client.h"
 #include "Mode.h"
-
-u16 srcport = 8160;                                     //source port
-
-u8 SocketIdForListen;                                   //Socket for Listening
-u8 SocketIdForclient = 0xff;                                   //Socket for Listening
-
-u8 socket[WCHNET_MAX_SOCKET_NUM];                       //Save the currently connected socket
-u8 SocketRecvBuf[WCHNET_MAX_SOCKET_NUM][RECE_BUF_LEN];  //socket receive buffer
-u8 MyBuf[RECE_BUF_LEN];
-u8 link_sock;
-/*********************************************************************
- * @fn      mStopIfError
- *
- * @brief   check if error.
- *
- * @param   iError - error constants.
- *
- * @return  none
- */
-void mStopIfError(u8 iError)
-{
-    if (iError == WCHNET_ERR_SUCCESS)
-        return;
-}
-
-/*********************************************************************
- * @fn      WCHNET_CreateTcpSocketListen
- *
- * @brief   Create TCP Socket for Listening
- *
- * @return  none
- */
-
-void WCHNET_CreateTcpSocketListen(void)
-{
-    u8 i;
-    SOCK_INF TmpSocketInf;
-    memset((void *) &TmpSocketInf, 0, sizeof(SOCK_INF));
-    TmpSocketInf.SourPort = 8160;
-    TmpSocketInf.ProtoType = PROTO_TYPE_TCP;
-    i = WCHNET_SocketCreat(&SocketIdForListen, &TmpSocketInf);
-    mStopIfError(i);
-    printf("server SocketId %d\r\n", SocketIdForclient);
-    i = WCHNET_SocketListen(SocketIdForListen);                   //listen for connections
-    mStopIfError(i);
-
-}
-
-/*********************************************************************
- * @fn      WCHNET_CreateTcpSocket
- *
- * @brief   Create TCP Socket
- *
- * @return  none
- */
-void WCHNET_CreateTcpSocket(void)
-{
-    u8 i;
-    SOCK_INF TmpSocketInf;
-    u8 ip_array[10];
-    Caven_Str_To_ip ("192.168.1.160",ip_array,4);
-    memset((void *) &TmpSocketInf, 0, sizeof(SOCK_INF));
-    memcpy((void *) TmpSocketInf.IPAddr, ip_array, 4);
-    TmpSocketInf.DesPort = 8000;
-    TmpSocketInf.SourPort = 8000;
-    TmpSocketInf.ProtoType = PROTO_TYPE_TCP;
-    TmpSocketInf.RecvBufLen = RECE_BUF_LEN;
-    i = WCHNET_SocketCreat(&SocketIdForclient, &TmpSocketInf);
-    printf("client SocketId %d\r\n", SocketIdForclient);
-    mStopIfError(i);
-    i = WCHNET_SocketConnect(SocketIdForclient);                        //make a TCP connection
-    mStopIfError(i);
-}
-
-/*********************************************************************
- * @fn      WCHNET_DataLoopback
- *
- * @brief   Data loopback function.
- *
- * @param   id - socket id.
- *
- * @return  none
- */
-void WCHNET_DataLoopback(u8 id)
-{
-#if 1
-    u8 i;
-    u32 len;
-    u32 endAddr = SocketInf[id].RecvStartPoint + SocketInf[id].RecvBufLen;       //Receive buffer end address
-
-    if ((SocketInf[id].RecvReadPoint + SocketInf[id].RecvRemLen) > endAddr) {    //Calculate the length of the received data
-        len = endAddr - SocketInf[id].RecvReadPoint;
-    }
-    else {
-        len = SocketInf[id].RecvRemLen;
-    }
-    i = WCHNET_SocketSend(id, (u8 *) SocketInf[id].RecvReadPoint, &len);        //send data
-    if (i == WCHNET_ERR_SUCCESS) {
-        WCHNET_SocketRecv(id, NULL, &len);                                      //Clear sent data
-    }
-#else
-    u32 len, totallen;
-    u8 *p = MyBuf, TransCnt = 255;
-
-    len = WCHNET_SocketRecvLen(id, NULL);                                //query length
-    printf("Receive Len = %d\r\n", len);
-    totallen = len;
-    WCHNET_SocketRecv(id, MyBuf, &len);                                  //Read the data of the receive buffer into MyBuf
-    while(1){
-        len = totallen;
-        WCHNET_SocketSend(id, p, &len);                                  //Send the data
-        totallen -= len;                                                 //Subtract the sent length from the total length
-        p += len;                                                        //offset buffer pointer
-        if( !--TransCnt )  break;                                        //Timeout exit
-        if(totallen) continue;                                           //If the data is not sent, continue to send
-        break;                                                           //After sending, exit
-    }
-#endif
-}
-
-/*********************************************************************
- * @fn      WCHNET_HandleSockInt
- *
- * @brief   Socket Interrupt Handle
- *
- * @param   socketid - socket id.
- *          intstat - interrupt status
- *
- * @return  none
- */
-u8 last_sock = 0;
-void WCHNET_HandleSockInt(u8 socketid, u8 intstat)
-{
-    u8 i;
-
-    if (intstat & SINT_STAT_RECV)                                 //receive data
-    {
-        stb_printf("socket[%d] stat recv\r\n",socketid);
-        WCHNET_DataLoopback(socketid);                            //Data loopback
-    }
-    if (intstat & SINT_STAT_CONNECT)                              //connect successfully
-    {
-#if KEEPALIVE_ENABLE
-        WCHNET_SocketSetKeepLive(socketid, ENABLE);
-#endif
-        WCHNET_ModifyRecvBuf(socketid, (u32) SocketRecvBuf[socketid],
-        
-        RECE_BUF_LEN);
-        if(SocketIdForclient != socketid)
-        {
-            if(last_sock != 0 && last_sock != socketid)
-            {
-                stb_printf("socket[%d] has been abandon\r\n",last_sock);
-                WCHNET_SocketClose(last_sock,TCP_CLOSE_ABANDON);
-            }
-            link_sock = socketid;
-            last_sock = socketid;
-        }
-        stb_printf("socket[%d] con\r\n",socketid);
-        for (i = 0; i < WCHNET_MAX_SOCKET_NUM; i++) {
-            if (socket[i] == 0xff) {                              //save connected socket id
-                socket[i] = socketid;
-                break;
-            }
-        }
-    }
-    if (intstat & SINT_STAT_DISCONNECT)                           //disconnect
-    {
-        for (i = 0; i < WCHNET_MAX_SOCKET_NUM; i++) {             //delete disconnected socket id
-            if (socket[i] == socketid) {
-                socket[i] = 0xff;
-                break;
-            }
-        }
-        stb_printf("socket[%d] discon\r\n",socketid);
-        if(SocketIdForclient == socketid)
-        {
-            SocketIdForclient = 0;
-        }
-    }
-    if (intstat & SINT_STAT_TIM_OUT)                              //timeout disconnect
-    {
-        for (i = 0; i < WCHNET_MAX_SOCKET_NUM; i++) {             //delete disconnected socket id
-            if (socket[i] == socketid) {
-                socket[i] = 0xff;
-                break;
-            }
-        }
-        stb_printf("socket[%d] over time\r\n",socketid);
-        if(SocketIdForclient == socketid)
-        {
-            SocketIdForclient = 0;
-        }
-    }
-}
-
-/*********************************************************************
- * @fn      WCHNET_HandleGlobalInt
- *
- * @brief   Global Interrupt Handle
- *
- * @return  none
- */
-void WCHNET_HandleGlobalInt(void)
-{
-    u8 intstat;
-    u16 i;
-    u8 socketint;
-    u8 socketid;
-    intstat = WCHNET_GetGlobalInt();                              //get global interrupt flag
-    if (intstat & GINT_STAT_UNREACH)                              //Unreachable interrupt
-    {
-        stb_printf("GINT_STAT_UNREACH\r\n");
-    }
-    if (intstat & GINT_STAT_IP_CONFLI)                            //IP conflict
-    {
-        stb_printf("GINT_STAT_IP_CONFLI\r\n");
-    }
-
-    if (intstat & GINT_STAT_PHY_CHANGE)                           //PHY status change
-    {
-        i = WCHNET_GetPHYStatus();
-        if (i & PHY_Linked_Status)
-        {
-            stb_printf("PHY Link Success\r\n");
-            SocketIdForclient = 0;
-            link_sock = 0;
-        }
-        else
-        {
-            stb_printf("PHY Link down\r\n");
-            if(link_sock)
-            {
-                stb_printf("eth link_sock %d \r\n",link_sock);
-                WCHNET_SocketClose(link_sock,TCP_CLOSE_ABANDON);
-                link_sock = 0;
-            }
-            if(SocketIdForclient)
-            {
-                stb_printf("eth link_sock %d \r\n",link_sock);
-                WCHNET_SocketClose(SocketIdForclient,TCP_CLOSE_ABANDON);
-                SocketIdForclient = 0xff;
-            }
-        }
-    }
-    if (intstat & GINT_STAT_SOCKET) {                             //socket related interru
-        for (i = 0; i < WCHNET_MAX_SOCKET_NUM; i++) 
-        {
-            socketint = WCHNET_GetSocketInt(i);
-            if (socketint)
-                WCHNET_HandleSockInt(i, socketint);
-        }
-    }
-}
 
 /*********************************************************************
  * @fn      main
@@ -281,11 +19,8 @@ void WCHNET_HandleGlobalInt(void)
  *
  * @return  none
  */
- uint8_t array[1000];
 int main(void)
 {
-    u8 i;
-    int addr = 0;
     Caven_BaseTIME_Type now_time;
     SystemCoreClockUpdate();
     Mode_Index();
@@ -296,13 +31,12 @@ int main(void)
     Mode_Use.TIME.Set_BaseTIME_pFun (now_time);
     stb_printf("MCU Init,MCU_SYS_FREQ: %d Hz \n",MCU_SYS_FREQ);
 
-    Base_ETH_Task_CallBcak_Bind(WCHNET_HandleGlobalInt);
     Base_ETH_config_local_ip (1,"192.168.1.168","192.168.1.1","255.255.255.0");
     Base_ETH_Init(1,ENABLE);
-    Mode_Use.TIME.Delay_S(3);
-    memset(socket, 0xff, WCHNET_MAX_SOCKET_NUM);
-    WCHNET_CreateTcpSocketListen();                               //Create TCP Socket for Listening
-    
+
+    Mode_Use.TIME.Delay_S(2);
+    Base_TCP_Server_Config ("8160",ENABLE);
+    Base_TCP_Client_Config ("192.168.1.160","8080",ENABLE);
     int old_sec = 0;
     while(1)
     {
@@ -312,11 +46,6 @@ int main(void)
             old_sec = now_time.SYS_Sec;
             // stb_printf("sys time utc: %d s \n",old_sec);
         }
-        if (SocketIdForclient == 0)
-        {
-            WCHNET_CreateTcpSocket();
-        }
         Base_ETH_Task ();
     }
 }
-
---
+++ DIY-Demo/OPEN_Cc/User/center_app.c
@@ -0,0 +1,194 @@
+#include "center_app.h"
+
+Caven_BaseTIME_Type Center_time;
+
+void debug_info_handle (void *data);
+void RFID_info_handle (void *data);
+void usb_info_handle (void *data);
+void server_info_handle (void *data);
+void client_info_handle (void *data);
+
+char JSON_array[500];
+int JSON_len = 0;
+uint8_t RFID_array[500];
+int RFIDBK_len = 0;
+Caven_BaseTIME_Type JSON_time,RFIDBK_time;
+
+int time_one = 0;
+int Center_State_machine(Caven_BaseTIME_Type time)
+{
+	int retval = 0,get_State = 0,temp_num = 0;
+    Center_time = time;
+	if (JSON_len)
+	{
+		temp_num = Center_time.SYS_Sec - JSON_time.SYS_Sec;
+		if (temp_num >= 0)
+		{
+			temp_num *= 1000000;
+			temp_num += Center_time.SYS_Us - JSON_time.SYS_Us;
+		}
+		if (JSON_len >= (sizeof(JSON_array) - 10))
+		{
+			Caven_app_JSON_Make_pack (JSON_array,TCP_HTTP_Link);
+			Mode_Use.UART.Send_Data_pFun (m_UART_CH2,(uint8_t *)JSON_array,JSON_len);
+			JSON_len = 0;
+		}
+		else if(temp_num > 2000)
+		{
+			Caven_app_JSON_Make_pack (JSON_array,TCP_HTTP_Link);
+			Mode_Use.UART.Send_Data_pFun (m_UART_CH2,(uint8_t *)JSON_array,JSON_len);
+			JSON_len = 0;
+		}
+	}
+	if (RFIDBK_len)
+	{
+		temp_num = Center_time.SYS_Sec - RFIDBK_time.SYS_Sec;
+		if (temp_num >= 0)
+		{
+			temp_num *= 1000000;
+			temp_num += Center_time.SYS_Us - RFIDBK_time.SYS_Us;
+		}
+		if (RFIDBK_len >= (sizeof(RFID_array) - 10))
+		{
+			Mode_Use.UART.Send_Data_pFun (DEBUG_OUT,(uint8_t *)RFID_array,RFIDBK_len);
+			RFIDBK_len = 0;
+		}
+		else if(temp_num > 2000)
+		{
+			Mode_Use.UART.Send_Data_pFun (DEBUG_OUT,(uint8_t *)RFID_array,RFIDBK_len);
+			RFIDBK_len = 0;
+		}
+	}
+	get_State |= Caven_app_State_machine (Center_time);
+#if SYS_BTLD == 0
+	// get_State |= GX_app_State_machine (Center_time);
+#endif
+	get_State |= System_app_State_machine (Center_time);
+	if(g_SYS_Config.Reset_falg)
+	{
+		retval |= 1;
+	}
+	return retval;
+}
+
+void Center_app_Init (void)
+{
+	Mode_Use.UART.Receive_Bind_pFun (DEBUG_OUT,debug_info_handle);
+	Mode_Use.UART.Receive_Bind_pFun (m_UART_CH2,RFID_info_handle);
+	Base_TCP_Server_Receive_Bind_Fun (server_info_handle);
+	Base_TCP_Client_Receive_Bind_Fun (client_info_handle);
+	// Mode_Use.USB_HID.RX_Callback_Bind(usb_info_handle);
+	Caven_app_Init ();
+#if SYS_BTLD == 0
+	// GX_app_Init ();
+#endif
+}
+
+int get_debug_data_num = 0,get_debug_pack_num = 0,get_debug_pack_error1 = 0;
+void debug_info_handle (void *data)
+{
+	uint8_t temp_data = *(uint8_t *)data;
+	int temp_num = 0;
+    temp_num = Caven_app_Make_pack (temp_data,SYS_Link,Center_time);
+#if SYS_BTLD == 0
+	// if (temp_num <= 0)
+	// {
+	// 	temp_num = GX_app_Make_pack (temp_data,SYS_Link,Center_time);
+	// }
+#endif
+	if (temp_num <= 0 && JSON_len < sizeof(JSON_array))
+	{
+		JSON_array[JSON_len++] = temp_data;
+		JSON_time = Center_time;
+	}
+	if(temp_num == 0xff)
+	{
+		g_SYS_Config.Connect_passage = SYS_Link;
+		get_debug_pack_num ++;
+	}
+	else if (temp_num == -1)
+	{
+		get_debug_pack_error1 ++;
+	}
+	
+	get_debug_data_num ++;
+}
+
+void usb_info_handle (void *data)
+{
+	uint8_t temp_data = *(uint8_t *)data;
+	int temp_num = 0;
+    temp_num = Caven_app_Make_pack (temp_data,USB_Link,Center_time);
+#if SYS_BTLD == 0
+	// if (temp_num <= 0)
+	// {
+	// 	temp_num = GX_app_Make_pack (temp_data,USB_Link,Center_time);
+	// }
+	if(temp_num == 0xff)
+	{
+		g_SYS_Config.Connect_passage = USB_Link;
+	}
+
+#endif
+}
+
+void server_info_handle (void *data)
+{
+	uint8_t temp_data = *(uint8_t *)data;
+	int temp_num = 0;
+    temp_num = Caven_app_Make_pack (temp_data,TCP_Server_Link,Center_time);
+#if SYS_BTLD == 0
+	// if (temp_num <= 0)
+	// {
+	// 	temp_num = GX_app_Make_pack (temp_data,USB_Link,Center_time);
+	// }
+	if(temp_num == 0xff)
+	{
+		g_SYS_Config.Connect_passage = TCP_Server_Link;
+	}
+
+#endif
+}
+
+void client_info_handle (void *data)
+{
+	uint8_t temp_data = *(uint8_t *)data;
+	int temp_num = 0;
+    temp_num = Caven_app_Make_pack (temp_data,TCP_Client_Link,Center_time);
+#if SYS_BTLD == 0
+	// if (temp_num <= 0)
+	// {
+	// 	temp_num = GX_app_Make_pack (temp_data,USB_Link,Center_time);
+	// }
+	if(temp_num == 0xff)
+	{
+		g_SYS_Config.Connect_passage = TCP_Client_Link;
+	}
+
+#endif
+}
+
+int get_RFID_data_num = 0,get_RFID_pack_num = 0,get_RFID_pack_error1 = 0;
+void RFID_info_handle (void *data)
+{
+#if SYS_BTLD == 0
+	uint8_t temp_data = *(uint8_t *)data;
+	int temp_num = 0;
+	
+	// temp_num = GX_app_Make_pack (temp_data,RS232_Link,Center_time);
+	if (temp_num <= 0 && RFIDBK_len < sizeof(RFID_array))
+	{
+		RFID_array[RFIDBK_len++] = temp_data;
+		RFIDBK_time = Center_time;
+	}
+	if(temp_num == 0xff)
+	{
+		get_RFID_pack_num ++;
+	}
+	else if (temp_num == -1)
+	{
+		get_RFID_pack_error1 ++;
+	}
+	get_RFID_data_num ++;
+#endif
+}
